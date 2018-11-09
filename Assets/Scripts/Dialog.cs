f510024762e18f76b0e211e6149c7ba1e792b2c2
andrej_hucko <hucko.andrej.mail@gmail.com>
2018-11-09 23:45:00 +0100

    simple working dialogues

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -1,13 +1,14 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Dialog : MonoBehaviour {
+public class Dialog : MonoBehaviour
+{
 
     // Of NPC
     public string name;
 
-    [TextArea(3,10)]
-    public string[] sentences; 
+    [TextArea(3, 10)]
+    public string[] sentences;
 }
--- Assets/Scripts/DialogTrigger.cs
+++ Assets/Scripts/DialogTrigger.cs
@@ -1,16 +1,17 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 // Triggers dialog we can create character and put this trigger on him
 
-public class DialogTrigger : MonoBehaviour {
+public class DialogTrigger : MonoBehaviour
+{
 
     public Dialog dialogue;
 
     private void TriggerDialogue()
     {
-        FindObjectOfType<DialogManger>().StartDialogue(dialogue);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 }
