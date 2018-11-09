6795f2cbf04aae0b85b51c1c1db3a2cc8288e766
andrej_hucko <hucko.andrej.mail@gmail.com>
2018-11-09 21:21:22 +0100

    camera + simple peasants movement

---
+++ Assets/CameraHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour {
+
+    bool gameIsReady = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!gameIsReady)
+        {
+            Quaternion finalRotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, finalRotation, 1);
+
+            if (Quaternion.Equals(transform.rotation, finalRotation))
+            {
+                gameIsReady = true;
+            }
+        }
+        else
+        {
+            GameObject[] people = GameObject.FindGameObjectsWithTag("People"); 
+            foreach (var peasant in people)
+            {
+                peasant.SetActive(true);
+            }
+
+        }
+
+    }
+}
---
+++ Assets/MovementHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementHandler : MonoBehaviour {
+
+    [SerializeField]
+    Transform destination;
+
+    public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector2.MoveTowards(transform.position, destination.position, speed);
+        // initialize dialog
+	}
+}
