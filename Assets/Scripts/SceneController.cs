7b14c0d6569cd20b7e99356c70acdd95419b7d06
Daidesi <andrej2024@gmail.com>
2018-11-10 16:30:11 +0100

    Add scene transitions.

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadSceneWithIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void LoadSceneWithName(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ Assets/Transition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour {
+
+    public float transitionTime = 5f;
+    public SceneController sceneController;
+
+    private float timer;
+
+    private void Start()
+    {
+        timer = Time.time + transitionTime;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (timer < Time.time)
+        {
+            sceneController.LoadNextScene();
+        }
+	}
+}
