0a4c3d85ad0a6365fece551a62df5ad17d83b476
andrej_hucko <hucko.andrej.mail@gmail.com>
2018-11-10 19:16:11 +0100

    FadeIn to next scene

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(1))
+        {
+            FadeToLevel(2);
+        }
+	}
+
+    public void FadeToLevel(int setLevel)
+    {
+        animator.SetTrigger("FadeIn");
+    }
+}
