893284cc0edcd03a714055aac9df38f62784e312
Daidesi <andrej2024@gmail.com>
2018-11-10 20:39:44 +0100

    Extend static data-handler.

--- Assets/Scripts/DataHandler.cs
+++ Assets/Scripts/DataHandler.cs
@@ -7,13 +7,117 @@ public static class DataHandler
 {
 
     private static List<Dialog> dialogJSONS = new List<Dialog>();
-    //private static int deaths, assists, points;
+    private static int followers = 5;
+    private static float godsAffection = 1;
+
+    private static bool wasInMinigame;
+    private static int numberOfDemons;
+    private static int killedDemons;
+    private static int day;
+    private static int weather;
+    private static int forecast;
 
     public static List<Dialog> DialogJSONS
     {
         get
         {
             return dialogJSONS;
+        }
+    }
+
+    public static int Followers
+    {
+        get
+        {
+            return followers;
+        }
+        set
+        {
+            followers = value;
+        }
+    }
+
+    public static float GodsAffection
+    {
+        get
+        {
+            return godsAffection;
+        }
+        set
+        {
+            godsAffection = value;
+        }
+    }
+
+    public static bool WasInMinigame
+    {
+        get
+        {
+            return wasInMinigame;
+        }
+        set
+        {
+            wasInMinigame = value;
+        }
+    }
+
+    public static int NumberOfDemons
+    {
+        get
+        {
+            return numberOfDemons;
+        }
+        set
+        {
+            numberOfDemons = value;
+        }
+    }
+
+    public static int KilledDemons
+    {
+        get
+        {
+            return killedDemons;
+        }
+        set
+        {
+            killedDemons = value;
+        }
+    }
+
+    public static int Day
+    {
+        get
+        {
+            return day;
+        }
+        set
+        {
+            day = value;
+        }
+    }
+
+    public static int Weather
+    {
+        get
+        {
+            return weather;
+        }
+        set
+        {
+            weather = value;
+        }
+    }
+
+    public static int Forecast
+    {
+        get
+        {
+            return forecast;
+        }
+        set
+        {
+            forecast = value;
         }
     }
 
@@ -36,5 +140,21 @@ public static class DataHandler
                 }
             }
         }
+    }
+
+    public static string getWeatherString(int number)
+    {
+        switch (number)
+        {
+            case 0:
+                return "Rainy";
+            case 1:
+                return "Windy";
+            case 2:
+                return "Sunny";
+            case 3:
+                return "Freezing";
+        }
+        return "Unknown weather";
     }
 }
--- Assets/Scripts/MinigameController.cs
+++ Assets/Scripts/MinigameController.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class MinigameController : MonoBehaviour {
 
+    public int minEnemies = 2;
+    public int enemiesCoeff = 4;
+    public float spawnRadius = 2.5f;
+    public int spawnAtBeginning = 3;
+
+    private int spawnedDemons = 0;
+    private int numberOfEnemies;
+
 	// Use this for initialization
 	void Start () {
-		
+        DataHandler.WasInMinigame = true;
+        var affection = DataHandler.GodsAffection;
+        numberOfEnemies =  minEnemies + (int) (Mathf.Round(enemiesCoeff / affection)) - enemiesCoeff;
+
+        DataHandler.NumberOfDemons = numberOfEnemies;
+
+        for (var i = 0; i < Mathf.Min(spawnAtBeginning, numberOfEnemies); i++)
+        {
+
+        }
 	}
 	
 	// Update is called once per frame
