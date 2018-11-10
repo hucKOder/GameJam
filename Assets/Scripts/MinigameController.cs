ae75ffe63871b2984e11930f25b98ff52f65ea40
Daidesi <andrej2024@gmail.com>
2018-11-10 21:00:35 +0100

    Add controller for minigame.

--- Assets/Scripts/MinigameController.cs
+++ Assets/Scripts/MinigameController.cs
@@ -8,9 +8,13 @@ public class MinigameController : MonoBehaviour {
     public int enemiesCoeff = 4;
     public float spawnRadius = 2.5f;
     public int spawnAtBeginning = 3;
+    public float spawnCoolDown = 1.5f;
+    public GameObject enemyPrefab;
 
     private int spawnedDemons = 0;
     private int numberOfEnemies;
+    private float spawnTimer;
+
 
 	// Use this for initialization
 	void Start () {
@@ -22,12 +26,22 @@ public class MinigameController : MonoBehaviour {
 
         for (var i = 0; i < Mathf.Min(spawnAtBeginning, numberOfEnemies); i++)
         {
-
+            Vector2 circle = Random.insideUnitCircle;
+            circle.Normalize();
+            Instantiate(enemyPrefab, circle, Quaternion.identity);
+            spawnedDemons++;
         }
+        spawnTimer = Time.time + spawnCoolDown;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (spawnedDemons < numberOfEnemies && spawnTimer < Time.time)
+        {
+            Vector2 circle = Random.insideUnitCircle;
+            circle.Normalize();
+            Instantiate(enemyPrefab, circle, Quaternion.identity);
+            spawnedDemons++;
+        }
 	}
 }
