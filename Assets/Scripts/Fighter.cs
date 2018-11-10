48504b5691ca940714b158822fd7a12959a1421a
Daidesi <andrej2024@gmail.com>
2018-11-10 10:22:30 +0100

    Add basic minigame.

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour {
+
+    public float dashTimeout = 0.3f;
+    public float dashMaxLength = 1f;
+    public float dashSpeed = 0.4f;
+
+    private float dashTimer;
+    private Vector2 dashPosition;
+
+    private bool isDashing = false;
+
+	// Use this for initialization
+	void Start () {
+        dashTimer = 0f;
+	}
+	
+	void FixedUpdate () {
+		if (dashTimer < Time.time && Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Plane plane = new Plane(Vector3.forward, new Vector3(0, 0, 0));
+            float distance;
+            if (plane.Raycast(ray, out distance))
+            {
+                Vector3 hitPoint = ray.GetPoint(distance);
+                if (Vector2.Distance(hitPoint, transform.position) > dashMaxLength)
+                {
+                    Vector3 direction = hitPoint - transform.position;
+                    direction.Normalize();
+                    dashPosition = transform.position + direction * dashMaxLength;
+                }
+                else
+                {
+                    dashPosition = hitPoint;
+                }
+                dashTimer = Time.time + dashTimeout;
+                isDashing = true;
+            }
+        }
+        if (isDashing)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, dashPosition, dashSpeed);
+            if (Equals(transform.position, dashPosition))
+            {
+                isDashing = false;
+            }
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            if (isDashing)
+            {
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
