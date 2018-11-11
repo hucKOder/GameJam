0103f244be3cf3824303b81d52550d643654e24c
Martin Hoang <martin.hoang@unicorn.com>
2018-11-11 08:49:34 +0100

    servant are amazing

---
+++ Assets/Scripts/ServantController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServantController : MonoBehaviour {
+
+	public Vector3 destination;
+	
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x <= destination.x && transform.position.y <= destination.y) { 
+			animator.Play("ser_idle");
+		}
+	}
+}
--- Assets/Scripts/ServantSpawner.cs
+++ Assets/Scripts/ServantSpawner.cs
@@ -7,20 +7,25 @@ public class ServantSpawner : MonoBehaviour {
 	public Transform destination; 
 	public GameObject servantPrefab;
 	public float radius = 2f;
-	
 
 	// Use this for initialization
 	void Start () {
-		for (var i = 0; i < DataHandler.Followers / 10; i++) {
+		StartCoroutine(Wait());
+	}
+
+	IEnumerator Wait()
+    {
+		for (var i = 0; i < 5; i++) {
 			var servant = Instantiate(servantPrefab, transform.position, transform.rotation);
 			var mh = servant.GetComponent<MovementHandler>();
 			var tempVect = new Vector2(destination.position.x, destination.position.y) + UnityEngine.Random.insideUnitCircle * radius;
 			mh.destination = new Vector3(tempVect.x, tempVect.y, 0f);
+			servant.GetComponent<ServantController>().destination = new Vector3(tempVect.x, tempVect.y, 0f);
+        	yield return new WaitForSecondsRealtime(1);
 		}
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 }
