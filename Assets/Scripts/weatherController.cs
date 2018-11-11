6234446787dcb793c17f53fdadcdf1502f978009
Martin Hoang <martin.hoang@unicorn.com>
2018-11-11 09:14:18 +0100

    I'ts RAINY DAYYYY ALEEEELUUUJA

---
+++ Assets/Scripts/weatherController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weatherController : MonoBehaviour {
+
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Debug.Log(DataHandler.Weather);
+		if (DataHandler.Weather == 0) {
+			anim.Play("church_rain");
+		}
+		else {
+			anim.Play("church_nothing");
+		}
+	}
+}
